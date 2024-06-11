3d9f1a3629ce0839ecd7bcd312ca0e463ad19959
Augustethegreat <chris@22onsloane.co>
2024-06-11 14:15:57 +0200

    push 3th

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import headerpic from "../image/banner_img.webp"
 import Image from 'next/image'
+import { Bars3Icon } from '@heroicons/react/16/solid'
 export default function Home() {
+
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [loginOpen, setLoginOpen] = useState(false);
   return (
     <div className='bg-[#fec800] w-full h-[700px]'>
       <div className='flex justify-center items-center mx-auto pt-4 text-[20px] font-normal gap-x-5'>
         <div className='bg-[#c12126] text-center rounded-[80px] justify-center items-center'>
             
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-black"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-11 " aria-hidden="true" />
+          </button>
         </div>
        <div className='justify-center items-center mx-auto pt-7 font-semibold hidden  lg:flex gap-x-[100px]'>
        <p>Products</p>
@@ -17,8 +32,8 @@ export default function Home() {
       </div>
 
       <div className='block md:flex justify-center items-center mx-auto mt-[100px] gap-x-10' >
-        <div className='mt-[-30px]'>
-        <p className='font-bold font-Compaign text-[50px] md:text-[80px] text-center md:text-start'>The Finest </p>
+        <div className='mt-[-30px] text-black'>
+        <p className='font-bold font-Compaign text-[50px]  md:text-[80px] text-center md:text-start'>The Finest </p>
         <p className='font-bold font-Compaign text-[50px] md:text-[80px] text-center md:text-start mt-[-10px] md:mt-[-50px]'>Spices</p>
         <div className='mt-0 pb-5 text-center md:text-start '>
         <p className='font-normal  text-[23px]'>Ethically sourced</p>
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
+"use client"
 import Image from "next/image";
 import Homes from "./components/Home";
 import cat1 from './image/cat_01.webp'
 import cat2 from './image/cat_02.webp'
 import cat3 from './image/cat_03.webp'
 import Recipe from "./components/Recipe";
+import { useState } from "react";
+import { ChatBubbleBottomCenterTextIcon, HomeIcon, PhoneIcon, RectangleGroupIcon, WalletIcon, XMarkIcon } from "@heroicons/react/16/solid";
+import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
 export default function Home() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [loginOpen, setLoginOpen] = useState(false);
   return (
   
       
 
       <div >
-      <p className='text-[#fff] bg-[#c12126] font-normal text-[22px] py-3 font-Compaign text-center '>Dont miss out! Follow us on social media for exclusive daily deals and updates!</p> 
+      <p className='text-[#fff] bg-[#c12126] font-normal text-[12px] md:text-[22px] py-3 font-Compaign text-center '>Dont miss out! Follow us on social media for exclusive daily deals and updates!</p> 
       <Homes/>
 
       <div className="w-full text-[#fff] block mt-[100px] md:mt-0 md:flex  text-[35px] font-semibold ">
@@ -56,6 +62,92 @@ export default function Home() {
         <div>
          <Recipe/>
         </div>
+
+
+
+
+        <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
+        <div className="fixed inset-0 z-10" />
+
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto shadow-[2px_3px_5px_black] bg-[#ffffff] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-black"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6 " aria-hidden="true" />
+            </button>
+          </div>
+
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6 ">
+        
+                <a
+                  href="/"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-[#f2f2f2] hover:text-[#45aad7]"
+                ><div className="flex gap-x-6 hover:text-[#45aad7]">
+                   <HomeIcon className="h-7 w-7 text-[#616f8d] mt-0 hover:text-white" aria-hidden="true"/>
+                   <p className="text-[#272828] font-medium text-[15px]">Home</p>
+                </div>
+               
+                </a>
+
+               
+                <a
+                 href="#offres"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-[#f2f2f2] hover:text-[#45aad7]"
+                ><div className="flex gap-x-6 hover:text-[#45aad7]" >
+                   <RectangleGroupIcon className="h-7 w-7 text-[#616f8d] mt-0 hover:text-white" aria-hidden="true"/>
+                   <p className="text-[#272828] font-medium text-[15px]"> Our Solutions</p>
+                </div>
+               
+                </a>
+                <a
+                 href="#offres"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-[#f2f2f2] hover:text-[#45aad7]"
+                ><div className="flex gap-x-6 hover:text-[#45aad7]" >
+                   <WalletIcon className="h-7 w-7 text-[#616f8d] mt-0 hover:text-white" aria-hidden="true"/>
+                   <p className="text-[#272828] font-medium text-[15px]"> Services</p>
+                </div>
+               
+                </a>
+                <a
+                
+                 href="#about"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-[#f2f2f2] hover:text-[#45aad7]"
+                ><div  className="flex gap-x-6 hover:text-[#45aad7]"  >
+                   <PhoneIcon className="h-7 w-7 text-[#616f8d] mt-0 hover:text-white" aria-hidden="true"/>
+                   <p className="text-[#272828] font-medium text-[15px]"> Get In Touch</p>
+                </div>
+               
+                </a>
+                <a
+                 href="#about"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-[#f2f2f2] hover:text-[#45aad7]"
+                ><div  className="flex gap-x-6 hover:text-[#45aad7]"  >
+                   <ChatBubbleBottomCenterTextIcon className="h-7 w-7 text-[#616f8d] mt-0 hover:text-white" aria-hidden="true"/>
+                   <p className="text-[#272828] font-medium text-[15px]"> About Us</p>
+                </div>
+               
+                </a>
+      
+              </div>
+
+              
+            
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
       </div>
       
     
