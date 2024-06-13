761b741440fe40c45af24533ed23fdb79f9b525c
Augustethegreat <chris@22onsloane.co>
2024-06-13 14:50:03 +0200

    push it

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -2,28 +2,117 @@
 import React, { useState } from 'react'
 import headerpic from "../image/banner_img.webp"
 import Image from 'next/image'
-import { Bars3Icon } from '@heroicons/react/16/solid'
+import { Bars3Icon, ChatBubbleBottomCenterTextIcon, HomeIcon, PhoneIcon, RectangleGroupIcon, WalletIcon, XMarkIcon } from '@heroicons/react/16/solid'
 import { motion } from 'framer-motion'
 import Link from 'next/link'
+import logo from "../image/logospice.jpg"
+import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
 export default function Home() {
 
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const [loginOpen, setLoginOpen] = useState(false);
+  const urlpic = "https://thespiceshop.co.uk/cdn/shop/files/logo_new.png?v=1674147259&width=352";
   return (
     <div className='bg-[#fec800] w-full h-[700px]'>
       <p className='text-[#fff] bg-[#c12126] font-normal text-[12px] md:text-[22px] py-3 font-Compaign text-center '>Dont miss out! Follow us on social media for exclusive daily deals and updates!</p>   
         
-      <div className='flex justify-center items-center mx-auto pt-0 text-[20px] font-normal gap-x-5'>
-       <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-black"
-            onClick={() => setMobileMenuOpen(true)}
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-11 " aria-hidden="true" />
-          </button>
-        </div>
+      <div >
+      <div className="flex items-center justify-between px-3 pt-3 lg:hidden">
+            <a href="#" className="-m-1.5 p-1.5 text-white">
+            <img
+              className="w-20 h-auto"
+              src={urlpic} 
+              alt=""
+            />
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-black"
+              onClick={() => setMobileMenuOpen(true)}
+            >
+              <span className="sr-only">Close menu</span>
+              <Bars3Icon className="h-6 w-6 " aria-hidden="true" />
+            </button>
+          </div>
+
+        <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
+        <div className="fixed inset-0 z-10" />
+
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto shadow-[2px_3px_5px_black] bg-[#c12126] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5 text-white">
+            <Image
+              className="w-20 h-auto"
+              src={logo}
+              alt=""
+            />
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-black"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6 " aria-hidden="true" />
+            </button>
+          </div>
+
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6 ">
+        
+                <a
+                  href="/"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-[#f2f2f2] hover:text-[#45aad7]"
+                ><div className="flex gap-x-6 hover:text-[#45aad7]">
+                   <HomeIcon className="h-7 w-7 text-[#fff] mt-0 hover:text-white" aria-hidden="true"/>
+                   <p className="text-[#fff] font-medium text-[15px]">Home</p>
+                </div>
+               
+                </a>
+
+                <a
+                 href="#offres"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-[#f2f2f2] hover:text-[#45aad7]"
+                ><div className="flex gap-x-6 hover:text-[#45aad7]"  >
+                   <RectangleGroupIcon className="h-7 w-7 text-[#fff] mt-0 hover:text-white" aria-hidden="true"/>
+                   <p className="text-[#fff] font-medium text-[15px]">Product</p>
+                </div>
+               
+                </a>
+                <a
+                 href="#offres"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-[#f2f2f2] hover:text-[#45aad7]"
+                ><div className="flex gap-x-6 hover:text-[#45aad7]"  >
+                   <WalletIcon className="h-7 w-7 text-[#fff] mt-0 hover:text-white" aria-hidden="true"/>
+                   <p className="text-[#fff] font-medium text-[15px]">About Us</p>
+                </div>
+                </a>
+                <a
+                 href="#about"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-medium leading-7 text-white hover:bg-[#f2f2f2] hover:text-[#45aad7]"
+                ><div  className="flex gap-x-6 hover:text-[#45aad7]"  >
+                   <PhoneIcon className="h-7 w-7 text-[#fff] mt-0 hover:text-white" aria-hidden="true"/>
+                   <p className="text-[#fff] font-medium text-[15px]">Contact Us</p>
+                </div>
+               
+                </a>
+      
+              </div>
+
+              
+            
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+
+
        <div className='justify-center items-center mx-auto py-4 font-semibold hidden  lg:flex gap-x-[100px]'>
        <Link href='/'>
        <p>Home</p>
